e324439b417e3fa97ae5ee73669c76a2c94ec9ff
Jayjay <jessiejeffyambao06@gmail.com>
2025-06-10 11:42:39 +0800

    june 10

---
+++ day1.js
@@ -0,0 +1,18 @@
+// DAY 1 basics of js  
+// onsole.log(`hello`);
+// console.log(`I like pizza`);
+
+// window.alert(`this is an alert!`);
+// window.alert(`i like pizza`);
+
+document.getElementById("myH1").textContent = `Hello`;
+document.getElementById("myP").textContent = `i like u!`;
+// this is a comment
+
+ /*      
+    This
+    is  
+    a
+    comment   
+ */
+
---
+++ day2.js
@@ -0,0 +1,26 @@
+// variable = A container that stores a value. Behaves as if it were the value it contains.
+
+// 1. declaration  let x;
+// 2. assignment   x = 100;
+
+let x;
+x = 100;
+
+console.log(x);
+
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(`You are ${age} years old`);
+console.log(`The price is ${price}`);
+console.log(`Your gpa is: ${gpa}`);
+
+
+// Data Type
+
+let firstName = "Bro";
+
+console.log(typeof firstName);
+console.log(firstName);
+console.log(`Your name is ${firstName}`);
